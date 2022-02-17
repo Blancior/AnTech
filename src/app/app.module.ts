3094d3938f0a42ea71f112f863d522e7a4e66839
Przemysław Kędziora <pkedziora347@gmail.com>
2022-02-17 17:32:41 +0100

    Fixed base routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {MatIconModule} from "@angular/material/icon";
+import { MainPageComponent } from './core/main-page/main-page.component';
+import {CoreModule} from "./core/core-module";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
+    CoreModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatIconModule
---
+++ src/app/core/core-module.ts
@@ -0,0 +1,11 @@
+import {CommonModule} from "@angular/common";
+import {MainPageComponent} from "./main-page/main-page.component";
+import {NgModule} from "@angular/core";
+
+
+@NgModule({
+  imports:  [
+    CommonModule
+  ],
+})
+export class CoreModule { }
